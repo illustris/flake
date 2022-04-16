bb5dad1114312268dc80e6cd13b17783fe5120ef
illustris <rharikrishnan95@gmail.com>
2022-04-16 10:03:23 +0530

    add fpga devshell

--- flake.nix
+++ flake.nix
@@ -3,10 +3,11 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 	};
 
-	outputs = { self, nixpkgs }: {
-		packages.x86_64-linux = let
-			legacyPackages = import nixpkgs { system = "x86_64-linux"; };
-		in import pkgs/all_packages.nix { pkgs = legacyPackages; };
-		#defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+	outputs = { self, nixpkgs }: let legacyPackages = import nixpkgs { system = "x86_64-linux"; }; in {
+		packages.x86_64-linux =  import pkgs/all_packages.nix { pkgs = legacyPackages; };
+
+		devShells.x86_64-linux.fpga = legacyPackages.mkShell { packages = with self.packages.x86_64-linux; [
+			oss-cad-suite-bin vhd2vl
+		];};
 	};
 }
