109c78c4e075184514f44469255c1e9c2195d2cf
illustris <rharikrishnan95@gmail.com>
2023-02-25 03:31:42 +0530

    fix eval error on riscv64

--- pkgs/fzpass/default.nix
+++ pkgs/fzpass/default.nix
@@ -1,8 +1,15 @@
 { writeScriptBin, lib, bash, fzf, ... }:
 # TODO: fix xdotool click to enter
-writeScriptBin "fzpass" (
+(writeScriptBin "fzpass" (
 	lib.replaceStrings
 		[ "/usr/bin/env bash" "fzf" ]
 		[ "${bash}/bin/bash" "${fzf}/bin/fzf" ]
 		(builtins.readFile ./fzpass)
-)
+)).overrideAttrs (old: {
+	meta = old.meta // {
+		platforms = [
+			"x86_64-linux"
+			"aarch64-linux"
+		];
+	};
+})
