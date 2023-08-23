57e3d859a1cb58c3c1c2ff6e1beb2bf87564bc31
illustris <rharikrishnan95@gmail.com>
2023-08-23 10:12:01 +0530

    fix eval error in suckless overlay

--- overlays/suckless/default.nix
+++ overlays/suckless/default.nix
@@ -1,15 +1,15 @@
 { ... }:
-self: super: {
-	dwm = super.dwm.overrideAttrs (oldAttrs: {
-		src = self.pkgs.fetchFromGitHub {
+final: prev: {
+	dwm = prev.dwm.overrideAttrs (oldAttrs: {
+		src = final.pkgs.fetchFromGitHub {
 			owner = "illustris";
 			repo = "dwm";
 			rev = "7df55abebad6a70236a6d6fc62fd475476fd77f6";
 			hash = "sha256-Cfdv+r271etL5nYkd4U2nRE/zCW7PaHkDC11eeGqLy4=";
 		};
 	});
-	st = super.st.overrideAttrs (oldAttrs: {
-		src = self.pkgs.fetchFromGitHub {
+	st = prev.st.overrideAttrs (oldAttrs: {
+		src = final.pkgs.fetchFromGitHub {
 			owner = "illustris";
 			repo = "st";
 			rev = "fa363487355fe0b27d82e7247577802ac66e4b0f";
