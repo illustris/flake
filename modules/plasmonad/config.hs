8439596f4f7bd22f941857c4ef27e023f3780ea8
illustris <rharikrishnan95@gmail.com>
2023-08-23 09:27:49 +0530

    add xmonad config

---
+++ modules/plasmonad/config.hs
@@ -0,0 +1,34 @@
+import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Config.Kde
+import XMonad.Util.Run
+import XMonad.Util.EZConfig -- This library provides the functions for managing keybindings
+import XMonad.Layout.GridVariants
+import XMonad.Layout.Tabbed
+
+import qualified XMonad.StackSet as W
+
+main = xmonad $ kdeConfig
+       { modMask = mod4Mask -- Use Super instead of Alt
+       , terminal = "st"
+       , manageHook = manageDocks <+> manageHook kdeConfig
+       , layoutHook = avoidStruts (Grid (16/10) ||| simpleTabbed ||| layoutHook kdeConfig)
+       , normalBorderColor  = "#808080" -- grey
+       , focusedBorderColor = "#00ccff" -- blue
+       -- more changes
+       }
+       `removeKeys`                -- Removed old keybindings
+       [ (mod4Mask .|. shiftMask, xK_Return)
+       , (mod4Mask, xK_Return)
+       , (mod4Mask, xK_m)
+       ]
+       `additionalKeys`
+       [ ((mod4Mask .|. shiftMask, xK_Return), windows W.swapMaster) -- make master
+       , ((mod4Mask .|. shiftMask, xK_space), withFocused $ windows . W.sink) -- retiling a floating window
+       , ((mod4Mask .|. controlMask, xK_m), windows W.focusMaster)
+       , ((mod4Mask, xK_Return), spawn "st") -- open terminal
+       , ((mod4Mask, xK_d), spawn "dmenu_run")
+       , ((mod4Mask, xK_m), sendMessage $ JumpToLayout "Full")
+       , ((mod4Mask, xK_g), sendMessage $ JumpToLayout "Grid")
+       , ((mod4Mask, xK_t), sendMessage $ JumpToLayout "Tabbed Simplest")
+       ]
