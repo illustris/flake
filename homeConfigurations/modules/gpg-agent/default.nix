24ac7f874138e271b850ff766c5d13bde9e80a79
illustris <rharikrishnan95@gmail.com>
2023-08-30 19:50:47 +0530

    enable ssh agent

--- homeConfigurations/modules/gpg-agent/default.nix
+++ homeConfigurations/modules/gpg-agent/default.nix
@@ -1,9 +1,10 @@
 { ... }: {
 	programs.gpg.enable = true;
 	services.gpg-agent = {
-		enable = true;
 		defaultCacheTtl = 60*60*24;
 		defaultCacheTtlSsh = 60*60*24;
+		enable = true;
 		extraConfig = "auto-expand-secmem";
+		enableSshSupport = true;
 	};
 }
