c5e93fe906900581aa396e1aef50e3187c43a839
illustris <rharikrishnan95@gmail.com>
2023-08-30 23:24:11 +0530

    enable ssh agent for real this time

--- homeConfigurations/modules/gpg-agent/default.nix
+++ homeConfigurations/modules/gpg-agent/default.nix
@@ -1,5 +1,9 @@
 { ... }: {
-	programs.gpg.enable = true;
+	programs = {
+		# this is required for HM to set GPG and SSH agent env vars
+		bash.enable = true;
+		gpg.enable = true;
+	};
 	services.gpg-agent = {
 		defaultCacheTtl = 60*60*24;
 		defaultCacheTtlSsh = 60*60*24;
