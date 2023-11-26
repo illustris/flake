8a20172f1ec8e3d22bd1d8ffec82f31864937e83
illustris <rharikrishnan95@gmail.com>
2023-11-26 23:38:51 +0530

    nur: remove unused input

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
 			url = "github:nix-community/home-manager";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
-		nur.url = "github:nix-community/NUR";
 		firefox-addons = {
 			url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
 			inputs.nixpkgs.follows = "nixpkgs";
